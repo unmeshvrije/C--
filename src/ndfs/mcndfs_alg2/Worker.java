898bf832a5836cd4b0e30599b3f37c3a5376e1ca
Luuk Verweij <luuk_verweij@msn.com>
2013-09-17 00:15:52 +0200

    Fixed some problems related to MapWithDefaultValues.java

--- src/ndfs/mcndfs_alg2/Worker.java
+++ src/ndfs/mcndfs_alg2/Worker.java
@@ -9,29 +9,29 @@ import graph.State;
 import ndfs.Result;
 import ndfs.CycleFound;
 import ndfs.NoCycleFound;
-import ndfs.mcndfs_alg2.Color;
+import ndfs.mcndfs_alg2.MapWithDefaultValues;
 
 class Worker implements Runnable
 {
     // TODO: decide scope
     private final Graph graph;
     private State state;
-    private Colors colors;
+    private MapWithDefaultValues<State,Color> colors;
 
     public Worker(final Graph graph)
     {
-    	this.graph = graph;
+        this.graph = graph;
         this.state = graph.getInitialState();
-	this.colors = new Colors(new HashMap<State,Color>());
+        this.colors = new MapWithDefaultValues<State,Color>(new HashMap<State,Color>(),Color.WHITE);
     }
 
     private void dfsRed(State s) throws Result {
         for (State t : graph.post(s)) {
-            if (colors.hasColor(t, Color.CYAN)) {
+            if (colors.hasKeyValuePair(t, Color.CYAN)) {
                 throw new CycleFound();
             }
-            else if (colors.hasColor(t, Color.BLUE)) {
-                colors.color(t, Color.RED);
+            else if (colors.hasKeyValuePair(t, Color.BLUE)) {
+                colors.setValue(t,Color.RED); // TODO complicated stuff here!!!
                 dfsRed(t);
             }
         }
@@ -39,30 +39,30 @@ class Worker implements Runnable
 
 
     private void dfsBlue(State s) throws Result {
-        colors.color(s, Color.CYAN);
+        colors.setValue(s, Color.CYAN);
         for (State t : graph.post(s)) {
-            if (colors.hasColor(t, Color.WHITE)) {
+            if (colors.hasKeyValuePair(t, Color.WHITE)) {
                 dfsBlue(t);
             }
         }
         if (s.isAccepting()) {
             dfsRed(s);
-            colors.color(s, Color.RED);
+            colors.setValue(s, Color.RED);
         }
         else {
-            colors.color(s, Color.BLUE);
+            colors.setValue(s, Color.BLUE);
         }
     }
 
     public void run()
     {
         try {
-	    dfsBlue(state);
-	    throw new NoCycleFound();
-	} catch (Result e) {
-	    // TODO: Set a flag
-	    System.out.println("We got : " + e.toString());
-	}
+            dfsBlue(state);
+        throw new NoCycleFound();
+        } catch (Result e) {
+            // TODO: Set a flag
+            System.out.println("We got : " + e.toString());
+        }
     }
 
 }
