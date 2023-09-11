bcb5419d9482eccbc5d00b3fd8744654e28a50e1
unmeshvrije <unmesh.joshi126@gmail.com>
2023-09-11 23:39:03 +0200

    chnages

--- Python/youtube.py
+++ Python/youtube.py
@@ -17,7 +17,7 @@ except:
 
 try:
     yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-except e:
+except Exception as e:
   print(f"Error {e}")
 print('Downloaded!')
 
